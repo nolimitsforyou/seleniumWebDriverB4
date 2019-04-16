6d9b7eac4858fbe8d14470287533476c2b5356f1
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-04-17 00:12:14 +0300

    test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,2 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SimpleTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void test() {
+        driver =  new ChromeDriver();
+        driver.get("https://www.airbnb.ru/");
+    }
 }
