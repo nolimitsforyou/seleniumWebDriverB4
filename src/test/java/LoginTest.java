e75bcab71696bfa2c7ce22460c461292fe8bb287
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-04-17 01:59:10 +0300

    logintest

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void test() {
+        driver =  new ChromeDriver();
+        driver.get(" http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.quit();
+    }
+}
