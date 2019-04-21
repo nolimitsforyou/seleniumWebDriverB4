3749a797966da1f2db328c5e08b5fee4354313df
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-04-22 01:38:02 +0300

    testAdmins

--- src/test/java/SidebarTest.java
+++ src/test/java/SidebarTest.java
@@ -2,10 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import sun.awt.windows.WEmbeddedFrame;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -13,10 +13,12 @@ import java.util.concurrent.TimeUnit;
 public class SidebarTest {
 
     private WebDriver driver;
-
+    private By mainSections = By.xpath("//li[@id = 'app-']");
+    private By subSections = By.xpath("//li[@id = 'app-']/ul/li/a");
+    private By headLine = By.xpath("//td[@id = 'content']/h1");
 
     @BeforeTest
-    public void start() {
+    public void setUp() {
         driver =  new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -28,9 +30,12 @@ public class SidebarTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        List<WebElement> mainSections = driver.findElements(By.xpath("//li[@id = 'app-']"));
-        for(int i = 0; i < mainSections.size(); i++) {
-            mainSections.get(i).click();
+        List<WebElement> mainS = driver.findElements(mainSections);
+        for (WebElement element:mainS) {
+            element.click();
+            WebElement headL = driver.findElement(headLine);
+            Assert.assertTrue(headL.isDisplayed());
+            if (driver.findElement(subSections).isDisplayed()) {}
         }
     }
 
