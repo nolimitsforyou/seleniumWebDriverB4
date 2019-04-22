6cb1e4ec786cd44085f7813caba8c878ff2fffeb
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-04-23 02:15:38 +0300

    sticker test

--- src/test/java/StickerTest.java
+++ src/test/java/StickerTest.java
@@ -1,33 +1,47 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class StickerTest {
+
     private WebDriver driver;
+
+    private By product = By.className("image-wrapper");
+    private By sticker = By.className("sticker");
+
 
     @BeforeTest
     public void setUp() {
-        driver =  new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void test() {
-
-    }
-
-    @AfterTest
-    public void stop() {
-        if(driver != null) {
-            driver.quit();
+        driver.get("http://localhost/litecart/en/");
+        List<WebElement> products = driver.findElements(product);
+        for(WebElement element : products){
+            Assert.assertEquals( element.findElements(sticker).size(), 1);
         }
     }
-}
+
+        @AfterTest
+        public void stop() {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
 
