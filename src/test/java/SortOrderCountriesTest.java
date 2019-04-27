1c509745fc4e8037476c720d380ca1d34c9c4782
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-04-27 15:33:41 +0300

    sortingTest

--- src/test/java/SortOrderCountriesTest.java
+++ src/test/java/SortOrderCountriesTest.java
@@ -1,14 +1,15 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -16,12 +17,15 @@ import java.util.concurrent.TimeUnit;
 public class SortOrderCountriesTest {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     private By countriesForm = By.xpath("//form[@name = 'countries_form']");
+    private By geozonesForm = By.xpath("//form[@name = 'geo_zones_form']");
     private By countriesButton = By.xpath("//span[contains(text(), 'Countries')]");
+    private By geoZonesButton = By.xpath("//span[contains(text(), 'Geo Zones')]");
     private By countryLink = By.xpath(".//tr[@class = 'row']//a[not(contains(@title ,'Edit'))]");
     private By editPen = By.xpath("./following-sibling::td");
-    private By countOfGeoZones = By.xpath(".//tr[@class = 'row']/td[6]");
+    private By countOfGeoZones = By.xpath("//tr[@class = 'row']/td[6]");
     private By zonesTable = By.xpath("//table[@id = 'table-zones']");
     private By zones = By.xpath(".//td[3]");
 
@@ -31,13 +35,32 @@ public class SortOrderCountriesTest {
         List<WebElement> listCountries = driver.findElement(mainElement).findElements(childElement);
         for (WebElement ele : listCountries) {
             originalList.add(ele.getText());
+            originalList.removeAll(Arrays.asList(""));
         }
         ArrayList<String> sortedList = new ArrayList<String>();
         for (String s : originalList) {
             sortedList.add(s);
+            sortedList.removeAll(Arrays.asList(""));
         }
         Collections.sort(sortedList);
         Assert.assertTrue(sortedList.equals(originalList));
+    }
+
+    private void chekCountriesWithZones(By countZones) {
+        List<WebElement> countries = driver.findElements(countZones);
+        for (int i = 0; i < countries.size(); i++){
+            countries = driver.findElements(countZones);
+            int countZone = Integer.parseInt(countries.get(i).getText());
+            if(countZone > 0) {
+                countries.get(i).findElement(editPen).click();
+                checkSortOrder(zonesTable, zones);
+                driver.findElement(countriesButton).click();
+            }
+        }
+    }
+
+    private void buttonClick(By buttonName) {
+        driver.findElement(buttonName).click();
     }
 
     @BeforeTest
@@ -45,6 +68,8 @@ public class SortOrderCountriesTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+
     }
 
     @Test
@@ -54,17 +79,20 @@ public class SortOrderCountriesTest {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         //а) проверить, что страны расположены в алфавитном порядке
-       // checkSortOrder(countriesForm, countryLink);
+        checkSortOrder(countriesForm, countryLink);
         //б) для тех стран, у которых количество зон отлично от нуля -- открыть страницу этой страны и там проверить, что зоны расположены в алфавитном порядке
-        List<WebElement> countriesWithZones = driver.findElement(countriesForm).findElements(countOfGeoZones);
-        for(WebElement ele : countriesWithZones){
-            int countGeo = Integer.parseInt(ele.getText());
-            if(countGeo > 0) {
-                ele.findElement(editPen).click();
-                //checkSortOrder(zonesTable, zones);
-                driver.findElement(countriesButton).click();
-            }
-        }
+        chekCountriesWithZones(countOfGeoZones);
+//        на странице зайти в каждую из стран и проверить, что зоны расположены в алфавитном порядке
+        buttonClick(geoZonesButton);
+        checkSortOrder(geozonesForm,countryLink);
+    }
+    @Test
+    public void test_2() {
+//        на странице зайти в каждую из стран и проверить, что зоны расположены в алфавитном порядке
+        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
 
