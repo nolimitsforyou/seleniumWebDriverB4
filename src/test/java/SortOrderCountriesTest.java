58473d18c8cfb6212bc07c8949f21f3c3ce00c36
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-05-02 17:07:37 +0300

    sorted rework

--- src/test/java/SortOrderCountriesTest.java
+++ src/test/java/SortOrderCountriesTest.java
@@ -18,7 +18,6 @@ public class SortOrderCountriesTest {
     private WebDriverWait wait;
 
     private By countriesForm = By.xpath("//form[@name = 'countries_form']");
-    private By editGeoZonePage = By.xpath("//table[@id = 'table-zones']");
     private By countriesButton = By.xpath("//span[contains(text(), 'Countries')]");
     private By geoZonesButton = By.xpath("//span[contains(text(), 'Geo Zones')]");
     private By countZonesByZonesPage = By.xpath("//tr[@class = 'row']/td[4]");
@@ -27,6 +26,7 @@ public class SortOrderCountriesTest {
     private By countOfGeoZones = By.xpath("//tr[@class = 'row']/td[6]");
     private By zonesTable = By.xpath("//table[@id = 'table-zones']");
     private By zones = By.xpath(".//td[3]");
+    private By zonesSelected = By.xpath(".//td[3]//option[@selected='selected']");
 
 
     private void checkSortOrder(By mainElement, By childElement){
@@ -83,7 +83,7 @@ public class SortOrderCountriesTest {
         checkCountriesWithZones(countOfGeoZones, zonesTable, zones, countriesButton, editPen);
 //        на странице зайти в каждую из стран и проверить, что зоны расположены в алфавитном порядке
         buttonClick(geoZonesButton);
-        checkCountriesWithZones(countZonesByZonesPage, );
+        checkCountriesWithZones(countZonesByZonesPage, zonesTable, zonesSelected, geoZonesButton, editPen);
     }
 
     @AfterTest
