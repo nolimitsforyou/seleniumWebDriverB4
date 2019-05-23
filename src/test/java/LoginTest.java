ff361d99ac33bd1cb6d03ea1b97bf424e3fc1538
nolimitsforyou <43932585+nolimitsforyou@users.noreply.github.com>
2019-05-24 01:47:52 +0300

    checkLogs

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,19 +1,32 @@
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    private WebDriver driver;
-
     @Test
     public void test() {
-        driver =  new ChromeDriver();
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability(CapabilityType.PROXY, seleniumProxy);
+        WebDriver driver = new ChromeDriver(options);
+        proxy.newHar();
         driver.get("http://localhost/litecart/admin");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+        Har har = proxy.endHar();
+        har.getLog().getEntries().forEach(l -> System.out.println(l.getResponse().getStatus() + ":" + l.getRequest().getUrl()));
         driver.quit();
     }
 }
